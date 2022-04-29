72bbdab213ba3e9a411e7f9f7ec0813521e749fd
Tom Linker <thomas.linker@itv.com>
2022-04-29 17:23:44 +0100

    Removed unused case class ThumbnailResponse

--- src/main/scala/it/itvcoretechtest/service/package.scala
+++ src/main/scala/it/itvcoretechtest/service/package.scala
@@ -3,5 +3,4 @@ package it.itvcoretechtest
 package service {
   trait ChecksumValidationFailure extends ThumbnailAppError
   case object InvalidChecksum extends ChecksumValidationFailure
-  case class Other() extends ChecksumValidationFailure
 }
